eeb9f0ca4bc97b9806a248005b9f6542ca754850
fazalrasel <fazalrasel@gmail.com>
2017-09-05 02:11:45 +0600

    bugFix: as the reducer's logic is refactored

--- src/reducer.spec.js
+++ src/reducer.spec.js
@@ -1,13 +1,12 @@
 import expect from 'expect';
 import reducer from './reducer';
-import { toggleLanguage } from './actions';
+import toggleLanguage from './actions';
 describe('reducer', () => {
   it('should have default currentLanguage en', () => {
     expect(reducer(undefined, { type: 'Nothing' }).currentLanguage).toEqual('en');
   });
   it('should have change currentLanguage to bn when toggleAction Called', () => {
     expect(reducer(undefined, toggleLanguage()).currentLanguage).toEqual('bn');
-    expect(reducer(undefined, toggleLanguage()).currentLanguage).toEqual('en');
-    expect(reducer(undefined, toggleLanguage()).currentLanguage).toEqual('bn');
   });
 });
+
